88f31d32954d2bee84c1c9db612e9cd3c529e9c7
unknown <kitazawa@kitazawa.ad.tess.swh.jp>
2012-11-13 20:25:25 +0900

    3 comp

--- basic/3/3.c
+++ basic/3/3.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#define ENTER 13
+#define BS 8
 
 void main(void)
 {
 	char c[80];
-	gets(c);
-	printf(c);
+	int result;
+	int i = 0;
+	int i2;
+	//result = getch();
+	//printf("%d",result);
+	//return;
+	
+	memset(c, 0, 80);
+	
+	while((result=getch()) != ENTER) {
+		if(result == BS) {
+		    printf("\r");
+		    for(i2=0;i2<i;i2++) {
+		       printf(" "); 
+		    }
+			c[--i] = '\0';
+		} else {
+		  c[i++] = result;
+		}
+		printf("\r");		
+		for(i2=0;i2<i;i2++) {
+		   printf("*"); 
+		}
+	}
+	printf("%s",c);
 }
