161f2fb5391d758328e4e7bc07297b70d3e8525a
unknown <kitazawa@kitazawa.ad.tess.swh.jp>
2012-11-14 15:50:15 +0900

    7 halt

--- basic/7/7_1.c
+++ basic/7/7_1.c
@@ -4,14 +4,12 @@ int func(const void * a, const void * b);
 
 void main(void)
 {
-	int num[] = {5, 60, 50, 80, 1, 15, 70, 100, 35, 20};
-	//char *fruit[] = {"orange", "apple", "melon", "mango", "pineapple", "strawberry", "peach", "kiwi", "pear", "cherry"};
+	char *fruit[] = {"orange", "apple", "melon", "mango", "pineapple", "strawberry", "peach", "kiwi", "pear", "cherry"};
 	int length;
 	int i;
 	
-	length = sizeof num / sizeof num[0];
+	length = sizeof fruit / sizeof *fruit;
 	
-	//void qsort( void *base, size_t n, size_t size, int (*func)( const void *c1, const void *c2 ) )
 	qsort(num, length, sizeof(num[0]), func);
 	
 	for(i = 0; i < length; i++) {
