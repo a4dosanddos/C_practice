f6eaaffd587e9171726a9bae0147df873985dfdd
unknown <kitazawa@kitazawa.ad.tess.swh.jp>
2012-11-19 12:48:24 +0900

    5 comp, 2 edit

--- string/5/5.c
+++ string/5/5.c
@@ -5,26 +5,30 @@ void main(int argc, char *argv[])
 {
 	char *p;
 	int i, length;
-	
+
 	if(argv[1] == NULL) {
 		printf("パスを入力してください");
 		exit(1);
 	}
-	
+
 	p = argv[1];
 	length = strlen(p);
 
 	for(i = length - 1; i >= 0; i--) {
 		//printf("%c", p[i]);
 		if(p[i] == PERIOD) {
-			p[i] = '\0';
-			printf("入力されたパスは %s です\n", p);
-			printf("入力されたパスの拡張子は %s です\n", &p[i+1]);
+			p[i+1] = 't';
+			p[i+2] = 'e';
+			p[i+3] = 's';
+			p[i+4] = 's';
+			p[i+5] = '\0';
 			break;
 		}
 	}
 
 	if(i == -1) {
 		printf("拡張子がありません");
+	} else {
+		printf("変更後のファイルパスは %s です\n", p);
 	}
 }
